5b1a7196df8e668fc56aa8eaa4215169f15775a6
Jiancheng LI <jiancheng.li.2011@gmail.com>
2020-12-14 12:12:09 +0800

    Init, test database connection

---
+++ Week_07/mysql-master-slave/src/main/java/com/example/mysqlmasterslave/DataSourceConfig.java
@@ -0,0 +1,19 @@
+package com.example.mysqlmasterslave;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+//@Configuration
+//public class DataSourceConfig {
+////    @Bean(name = "masterDataSource")
+////    @Qualifier("primaryDataSource")
+////    @ConfigurationProperties(prefix = "spring.datasource.master")
+////    public DataSource masterDataSource() {
+////        return DataSourceBuilder.create().build();
+////    }
+//}
